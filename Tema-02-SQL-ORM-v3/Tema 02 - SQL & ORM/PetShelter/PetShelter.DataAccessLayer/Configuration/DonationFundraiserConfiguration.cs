077db79ef944498296e591bbd49d648d401ef205
Laurentiu-Marian <laurmarian46@gmail.com>
2023-02-28 23:35:27 +0200

    Add Tema2 V3

---
+++ Tema-02-SQL-ORM-v3/Tema 02 - SQL & ORM/PetShelter/PetShelter.DataAccessLayer/Configuration/DonationFundraiserConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetShelter.DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShelter.DataAccessLayer.Configuration
+{
+    public class DonationFundraiserConfiguration : IEntityTypeConfiguration<DonationFundraiser>
+    {
+        public void Configure(EntityTypeBuilder<DonationFundraiser> builder)
+        {
+            builder.HasKey(p => new { p.Id, p.DonationId, p.FundraiserId });
+
+            builder.HasOne(p => p.donation).WithMany(p => p.donationFundraisers).HasForeignKey(p => p.DonationId).IsRequired();
+
+            builder.HasOne(p => p.fundraiser).WithMany(p => p.fundraisersDonation).HasForeignKey(p => p.FundraiserId).IsRequired();
+        }
+    }
+}
---
+++ Tema-02-SQL-ORM-v3/Tema 02 - SQL & ORM/PetShelter/PetShelter.DataAccessLayer/Models/DonationFundraiser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShelter.DataAccessLayer.Models
+{
+    public class DonationFundraiser : IEntity
+    {
+        public int Id { get; set; }
+        public int DonationId { get; set; }
+        public Donation donation { get; set; }
+        public int FundraiserId { get; set; }
+        public Fundraiser fundraiser { get;set; }
+
+        public DonationFundraiser(int donationId, int fundraiserId) 
+        {
+            DonationId = donationId;
+            FundraiserId = fundraiserId;
+        }
+
+    }
+}
